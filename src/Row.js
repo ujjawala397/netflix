09b2963c190c045c902499997ebcf45fd66a7867
ujjawal <ujjawala397@gmail.com>
2021-06-19 00:47:28 +0530

    heading colour updated

--- src/Row.js
+++ src/Row.js
@@ -42,7 +42,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
   return (
     <div className="row">
-      <h2>{title}</h2>
+      <h2 className="heading">{title}</h2>
 
       <div className="row_posters">
         {movies.map((movie) => (
